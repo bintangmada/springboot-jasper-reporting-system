f78e6c0b56362875343c3300aa4df01e75ff50cc
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-03-27 15:41:11 +0700

    feat: add ReportService for PDF and Excel export via JasperReports

---
+++ src/main/java/com/bintang/springboot_jasper_reporting_system/service/ReportService.java
@@ -0,0 +1,86 @@
+package com.bintang.springboot_jasper_reporting_system.service;
+
+import com.bintang.springboot_jasper_reporting_system.entity.Employee;
+import com.bintang.springboot_jasper_reporting_system.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportService {
+
+    private static final Logger log = LoggerFactory.getLogger(ReportService.class);
+    private final EmployeeRepository employeeRepository;
+
+    public ReportService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    private JasperPrint generateJasperPrint() throws Exception {
+        // Load and compile the .jrxml template
+        InputStream reportStream = ResourceUtils.getURL("classpath:reports/employee_report.jrxml").openStream();
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
+
+        // Fetch data from database
+        List<Employee> employees = employeeRepository.findAll();
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+
+        // Set parameters
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("reportTitle", "PT. Bintang Mada Corporation - Employee Data");
+        parameters.put("generatedDate",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm:ss")));
+
+        // Fill the report
+        return JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+    }
+
+    /**
+     * Export employee report as PDF bytes.
+     */
+    public byte[] exportPdf() throws Exception {
+        log.info("Generating Employee Report (PDF)...");
+        JasperPrint jasperPrint = generateJasperPrint();
+        return JasperExportManager.exportReportToPdf(jasperPrint);
+    }
+
+    /**
+     * Export employee report as Excel (.xlsx) bytes.
+     */
+    public byte[] exportExcel() throws Exception {
+        log.info("Generating Employee Report (Excel)...");
+        JasperPrint jasperPrint = generateJasperPrint();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        JRXlsxExporter exporter = new JRXlsxExporter();
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+
+        SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+        configuration.setOnePagePerSheet(false);
+        configuration.setRemoveEmptySpaceBetweenRows(true);
+        configuration.setDetectCellType(true);
+        configuration.setWhitePageBackground(false);
+        exporter.setConfiguration(configuration);
+
+        exporter.exportReport();
+
+        return outputStream.toByteArray();
+    }
+}
