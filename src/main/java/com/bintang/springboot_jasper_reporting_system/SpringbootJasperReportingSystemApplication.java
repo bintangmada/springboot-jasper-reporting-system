d080ff7b679d070522f1dd6e2581f77ae52fe45f
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-03-27 15:21:56 +0700

    init project

---
+++ src/main/java/com/bintang/springboot_jasper_reporting_system/SpringbootJasperReportingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.bintang.springboot_jasper_reporting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJasperReportingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJasperReportingSystemApplication.class, args);
+		System.out.println("SERVER IS RUNNING");
+	}
+
+}
---
+++ src/test/java/com/bintang/springboot_jasper_reporting_system/SpringbootJasperReportingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bintang.springboot_jasper_reporting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJasperReportingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
